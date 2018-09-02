93f67d173353581ee6091bdf3c99ddc810e719c5
Fernando Suarez <ferrrsj@gmail.com>
2018-09-02 20:24:21 +0200

    Added FPS counter.

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 #include <SDL2_image/SDL_image.h>
+#include <time.h>
 
 //Key press surfaces constants
 enum KeyPressSurfaces {
@@ -54,7 +55,7 @@ void End (void);
 void addShot (Ship *ship);
 void drawShots (Ship *ship, SDL_Surface *s);
 void deleteOutShots (Shot *shots, SDL_Surface *s );
-void HandleShipMovement (SDL_Event e);
+void HandleShipMovement (SDL_Event *e);
 void MoveShip (Ship *s);
 
 Ship s1 = {SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 0, 0, 2, 0.5, 0.5, NULL};
@@ -73,17 +74,20 @@ int main () {
     }
     
     SDL_bool quit = SDL_FALSE;
-    SDL_Event e;
+    SDL_Event *e = malloc(sizeof(SDL_Event));
     //const uint8_t *keyboardState = SDL_GetKeyboardState(NULL);
     shipCurrent = shipSurface[KEY_PRESS_SURFACE_DEFAULT];
     
+    clock_t t_ini, t_end;
+    int cnt = 250;
+    t_ini = clock();
     while (!quit) {
-        while (SDL_PollEvent(&e) != 0) {
+        while (SDL_PollEvent(e) != 0) {
             //User requests quit
-            if (e.type == SDL_QUIT) {
+            if (e->type == SDL_QUIT) {
                 quit = SDL_TRUE;
             }
-            if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_SPACE)
+            if (e->type == SDL_KEYDOWN && e->key.keysym.sym == SDLK_SPACE)
                 addShot(&s1);
             HandleShipMovement(e);
         }
@@ -97,6 +101,14 @@ int main () {
         drawShots(&s1, laserSurface);
         //Update the surface
         SDL_UpdateWindowSurface(window);
+        if (cnt == 0) {
+            t_end = clock();
+            double secs = (double)(t_end - t_ini) / CLOCKS_PER_SEC;
+            printf("FPS: %f\n", 250/secs);
+            cnt = 250;
+            t_ini = clock();
+        }
+        cnt--;
     }
     
     //Free resources and close SDL
@@ -107,6 +119,7 @@ int main () {
 
 SDL_bool Init () {
     SDL_bool success = SDL_TRUE;
+    //int i = SDLK_UP;
     
     // SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO|SDL_INIT_CDROM)
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -199,7 +212,7 @@ void addShot (Ship *ship) {
     shot->x = (ship->x + shipCurrent->w*ship->sw/2) - laserSurface->w/2*shot->sw;
     shot->y = ship->y;
     shot->vx = 0;
-    shot->vy = 13;
+    shot->vy = 5;
     shot->sw = shot->sh = 0.1;
     shot->next = NULL;
     
@@ -242,10 +255,10 @@ void deleteOutShots (Shot *shots, SDL_Surface *s) {
     }
 }
 
-void HandleShipMovement (SDL_Event e) {
-    if (e.type == SDL_KEYDOWN && e.key.repeat == 0) {
+void HandleShipMovement (SDL_Event *e) {
+    if (e->type == SDL_KEYDOWN && e->key.repeat == 0) {
         //Adjust the velocity
-        switch (e.key.keysym.sym) {
+        switch (e->key.keysym.sym) {
             case SDLK_UP: s1.vy -= s1.VEL; break;
             case SDLK_DOWN: s1.vy += s1.VEL; break;
             case SDLK_LEFT: s1.vx -= s1.VEL; break;
@@ -253,15 +266,16 @@ void HandleShipMovement (SDL_Event e) {
         }
     }
     //If a key was released
-    else if (e.type == SDL_KEYUP && e.key.repeat == 0) {
+    else if (e->type == SDL_KEYUP && e->key.repeat == 0) {
         //Adjust the velocity
-        switch (e.key.keysym.sym) {
+        switch (e->key.keysym.sym) {
             case SDLK_UP: s1.vy += s1.VEL; break;
             case SDLK_DOWN: s1.vy -= s1.VEL; break;
             case SDLK_LEFT: s1.vx += s1.VEL; break;
             case SDLK_RIGHT: s1.vx -= s1.VEL; break;
         }
     }
+    //printf("Vx=%d, Vy=%d\n",s1.vx, s1.vy);
 }
 
 void MoveShip (Ship *s) {
